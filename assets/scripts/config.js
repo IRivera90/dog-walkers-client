696b0afccec13182a38228d190c2469a1821bfa0
IRivera90 <isacsac90@gmail.com>
2020-01-31 10:00:20 -0500

    Added Auth files and code
    Added sign in, sign out, change password, and sign up code

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,8 +1,9 @@
 'use strict'
-
+// https://dog-walkers.herokuapp.com/
+// https://blooming-castle-95686.herokuapp.com/
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://blooming-castle-95686.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
